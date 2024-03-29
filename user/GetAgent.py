3ab8ba6428b981ee9319579e0b5d5b667497d9d3
Jakob Danninger <68190922+jdanninger@users.noreply.github.com>
2024-03-29 00:00:43 -0400

    updated readme

--- user/GetAgent.py
+++ user/GetAgent.py
@@ -1,19 +1,28 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
 class GetAgent:
+    #self.agent_id = ''
     """
     This class is used to get information about an agent
     for the front end like whether to enable text-to-speech
     or what model to use
     """
-    def __init__(agent_id, self) -> None:
-        self.agent_id = agent_id
+    def __init__(self):
+        self.agent_id = 'asdf'
+
 
     def getAgentProperties(self):
-        engine = create_engine(os.getenv("DB_URI"))
+
+        #engine = create_engine(os.getenv("DB_URI"))
+        engine = create_engine("postgresql+psycopg://postgres:admin@host.docker.internal:5432/ai4edu_local")
         with engine.connect() as conn:
             result = conn.execute(text("SELECT version FROM db_version"))
-            db_result = result.fetchone()[0]
-        
+            print(result)
+
+
+t = GetAgent()
+
+t.getAgentProperties()
 
