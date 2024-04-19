bfab0f753a642131bf9ad475c90c7e2d9a32e644
SamJin98 <sam_ruilin@hotmail.com>
2024-04-19 11:57:39 -0400

    change message to makes sense (#19)
    * change message to makes sense
    
    * fix: change sort order

--- admin/Thread.py
+++ admin/Thread.py
@@ -47,11 +47,10 @@ def get_thread_by_id(thread_id: UUID):
     try:
         thread_messages = message_handler.get_thread(str(thread_id))
         if not thread_messages:
-            return response(False, status_code=404, message="Thread not found")
+            return response(False, status_code=404, message="Thread messages not found")
 
         # Sort the messages by 'created_at' time in descending order
-        sorted_messages = sorted(thread_messages, key=lambda x: x.created_at,
-                                 reverse=True)
+        sorted_messages = sorted(thread_messages, key=lambda x: x.created_at)
         return response(True, data={"thread_id": thread_id,
                                     "messages": sorted_messages})
     except Exception as e:
