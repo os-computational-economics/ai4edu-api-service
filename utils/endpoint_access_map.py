c8fc404599f359a81f542a5c3d7bb88ce5b88393
Ruihuang Yang <rxy216@case.edu>
2024-05-24 21:01:19 -0400

    feat: add endpoint access definition map

---
+++ utils/endpoint_access_map.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2024.
+# -*-coding:utf-8 -*-
+"""
+@file: endpoint_access_map.py
+@author: Jerry(Ruihuang)Yang
+@email: rxy216@case.edu
+@time: 5/24/24 19:54
+"""
+endpoint_access_map = {
+    "/agent/{agent_id}": {"student": True, "teacher": True, "admin": True},
+    "/agents/add_agent": {"student": False, "teacher": True, "admin": True},
+    "/agents/delete_agent": {"student": False, "teacher": True, "admin": True},
+    "/agents/update_agent": {"student": False, "teacher": True, "admin": True},
+    "/agents/agents": {"student": False, "teacher": True, "admin": True},
+    "/agents/agent/{agent_id}": {"student": False, "teacher": True, "admin": True},
+    "/threads/get_thread/{thread_id}": {"student": False, "teacher": True, "admin": True},
+    "/threads/get_thread_list": {"student": False, "teacher": True, "admin": True},
+    "/sso": {"student": True, "teacher": True, "admin": True},
+    "/stream_chat": {"student": True, "teacher": True, "admin": True},
+    "/get_tts_file": {"student": True, "teacher": True, "admin": True},
+    "/get_temp_stt_auth_code": {"student": True, "teacher": True, "admin": True},
+    "/get_new_thread": {"student": True, "teacher": True, "admin": True},
+}
