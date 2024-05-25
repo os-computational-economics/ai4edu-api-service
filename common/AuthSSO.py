4e4623d040f5432084b0a2abbceca1a5bf5573fe
Ruihuang Yang <rxy216@case.edu>
2024-05-24 21:05:35 -0400

    change: remove name and id from redirect url because these info are in access token

--- common/AuthSSO.py
+++ common/AuthSSO.py
@@ -40,10 +40,10 @@ class AuthSSO:
                 user_auth = UserAuth()
                 user_id = user_auth.user_login(self.student_id, user_info)
                 refresh_token = user_auth.gen_refresh_token(user_id)
-                access_token = user_auth.gen_access_token(refresh_token, user_id)
+                access_token = user_auth.gen_access_token(refresh_token)
                 if user_id:
                     return RedirectResponse(
-                        url=f"{self.came_from}?user_id={user_id}&user_name={user_info['cn']}&refresh_token={refresh_token}&access_token={access_token}")
+                        url=f"{self.came_from}?refresh_token={refresh_token}&access_token={access_token}")
                 else:
                     return RedirectResponse(url=f"{self.came_from}?user_id=error")
             else:
