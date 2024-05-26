b1474b59c51e407e0b63adc4a900bb165c425a54
Ruihuang Yang <rxy216@case.edu>
2024-05-25 23:22:59 -0400

    fix: fix broken key

--- utils/token_utils.py
+++ utils/token_utils.py
@@ -5,9 +5,50 @@ from datetime import datetime, timedelta, timezone
 
 logger = logging.getLogger(__name__)
 
+
+def fix_key(broken_key):
+    """
+    Fix the broken key by removing 'n' every 64 characters, and reformatting the key
+    :param broken_key:
+    :return:
+    """
+    # Step 1: Find the header and footer
+    header_start = broken_key.find("-----BEGIN")
+    header_end = broken_key.find("-----", header_start + len("-----BEGIN")) + len("-----")
+    header = broken_key[header_start:header_end]
+
+    footer_start = broken_key.find("-----END")
+    footer_end = broken_key.find("-----", footer_start + len("-----END")) + len("-----")
+    footer = broken_key[footer_start:footer_end]
+
+    # Step 2: Extract the body between header and footer
+    body_start = header_end
+    body_end = footer_start
+    body = broken_key[body_start:body_end]
+
+    # Step 3: Remove 'n' every 64 characters in the body
+    body = body.replace('n', '')
+    body_chunks = [body[i:i + 64] for i in range(0, len(body), 64)]
+    formatted_body = '\\n'.join(body_chunks)
+
+    # Step 4: Assemble everything
+    fixed_key = f"{header}\\n{formatted_body}\\n{footer}"
+
+    return fixed_key
+
+
 private_key = os.getenv("JWT_PRIVATE_KEY")
+# if the key starts with a lower case n after the header, it is broken
+header_start = private_key.find("-----BEGIN")
+header_end = private_key.find("-----", header_start + len("-----BEGIN")) + len("-----")
+if private_key[header_end] == 'n':
+    private_key = fix_key(private_key)
 public_key = os.getenv("JWT_PUBLIC_KEY")
-print(public_key)
+# if the key starts with a lower case n after the header, it is broken
+header_start = public_key.find("-----BEGIN")
+header_end = public_key.find("-----", header_start + len("-----BEGIN")) + len("-----")
+if public_key[header_end] == 'n':
+    public_key = fix_key(public_key)
 algorithm = "RS256"
 
 
