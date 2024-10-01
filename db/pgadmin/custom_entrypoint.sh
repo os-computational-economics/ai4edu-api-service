be5283bff47e09421b8a4022ea003720693f8ce4
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-10-01 10:29:57 -0400

    fix github action formatter

--- db/pgadmin/custom_entrypoint.sh
+++ db/pgadmin/custom_entrypoint.sh
@@ -5,9 +5,9 @@ PGPASSFILE="$HOME/.pgpass"
 
 # Create the .pgpass file for password
 echo "Creating pgpass file at $PGPASSFILE"
-echo "${POSTGRES_ENDPOINT}:*:*:${POSTGRES_USER}:${POSTGRES_PASSWORD}" > "$PGPASSFILE"
-chmod 600 $PGPASSFILE
-cat $PGPASSFILE
+echo "${POSTGRES_ENDPOINT}:*:*:${POSTGRES_USER}:${POSTGRES_PASSWORD}" >"$PGPASSFILE"
+chmod 600 "$PGPASSFILE"
+cat "$PGPASSFILE"
 echo "pgpass file created successfully."
 
 echo "Creating servers.json file in $SERVERS_JSON_PATH"
@@ -17,7 +17,7 @@ sed \
   -e "s|\${POSTGRES_DB}|${POSTGRES_DB}|g" \
   -e "s|\${POSTGRES_USER}|${POSTGRES_USER}|g" \
   -e "s|\${PGPASSFILE}|${PGPASSFILE}|g" \
-  /pgadmin_init/servers.json.template > "$SERVERS_JSON_PATH"
+  /pgadmin_init/servers.json.template >"$SERVERS_JSON_PATH"
 
 echo "$SERVERS_JSON_PATH file created successfully."
 cat $SERVERS_JSON_PATH
